731ed23ed6aaba6b85f1196adf6c7fc9e5f2a2ef
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-11-23 22:56:03 +1100

    'added_classes_for_direction

---
+++ Snake-Game/Direction.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Snake_Game
+{
+    public class Direction
+    {
+        public int RowOffset { get; }
+        public int ColOffset { get; }
+
+        private Direction (int rowOffset, int colOffset)
+        {
+
+        }
+    }
+}
