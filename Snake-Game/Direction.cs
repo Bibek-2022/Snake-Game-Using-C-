62f65fc6c45147b4e587fab43e66416d81e37502
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-12-06 23:32:08 +1100

    'added_ascode_for_position'

--- Snake-Game/Direction.cs
+++ Snake-Game/Direction.cs
@@ -1,4 +1,7 @@
 ﻿
+using System;
+using System.Collections.Generic;
+
 namespace Snake_Game
 {
     public class Direction
@@ -19,6 +22,33 @@ namespace Snake_Game
         {
             RowOffset = rowOffset;
             ColOffset = colOffset;
+        }
+
+        public Direction Opposite()
+        {
+            return new Direction(-RowOffset, -ColOffset);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Direction direction &&
+                   RowOffset == direction.RowOffset &&
+                   ColOffset == direction.ColOffset;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(RowOffset, ColOffset);
+        }
+
+        public static bool operator ==(Direction left, Direction right)
+        {
+            return EqualityComparer<Direction>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Direction left, Direction right)
+        {
+            return !(left == right);
         }
     }
 }
